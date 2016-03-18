ae4d1b6ea6769a7d80082848f2a9b57d36f84f25
Lars Kuhtz <lakuhtz@gmail.com>
2016-03-17 19:21:50 -0700

    Update license, copyright, and maintainer

--- src/System/Logger/Internal.hs
+++ src/System/Logger/Internal.hs
@@ -1,26 +1,10 @@
--- Copyright (c) 2014-2015 PivotCloud, Inc.
---
--- System.Logger.Internal
---
--- Please feel free to contact us at licensing@pivotmail.com with any
--- contributions, additions, or other feedback; we would love to hear from
--- you.
---
--- Licensed under the Apache License, Version 2.0 (the "License"); you may
--- not use this file except in compliance with the License. You may obtain a
--- copy of the License at http://www.apache.org/licenses/LICENSE-2.0
---
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
--- WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
--- License for the specific language governing permissions and limitations
--- under the License.
-
 -- |
 -- Module: System.Logger.Internal
--- Copyright: Copyright (c) 2014-2015 PivotCloud, Inc.
+-- Copyright:
+--     Copyright (c) 2016 Lars Kuhtz <lakuhtz@gmail.com>
+--     Copyright (c) 2014-2015 PivotCloud, Inc.
 -- License: Apache License, Version 2.0
--- Maintainer: Lars Kuhtz <lkuhtz@pivotmail.com>
+-- Maintainer: Lars Kuhtz <lakuhtz@gmail.com>
 -- Stability: experimental
 --
 
